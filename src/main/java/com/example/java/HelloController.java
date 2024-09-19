51bef82a9c661e2e2d655b3b46a59e26e0360561
nitchakan87 <166984145+nitchakan87@users.noreply.github.com>
2024-09-19 16:51:16 +0700

    Update HelloController.java

--- src/main/java/com/example/java/HelloController.java
+++ src/main/java/com/example/java/HelloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,15 @@ import com.example.java.model.Order;
 
 import lombok.extern.slf4j.Slf4j;
 
+
 @RestController
 @Slf4j
 public class HelloController {
+    private   Logger log;
 
     @GetMapping("/orders")
     public List<Order> getOrders(@RequestHeader Map<String, String> RequestHeader) {
+
         log.info("request {}", RequestHeader);
         return Arrays.asList(
                 new Order(1L, "book", 1234.00),
