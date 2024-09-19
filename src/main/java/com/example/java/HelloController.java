89763761b21902f1dfdc1b6ed81c38786a8e8dcd
nitchakan87 <166984145+nitchakan87@users.noreply.github.com>
2024-09-19 17:37:53 +0700

    Update HelloController.java

--- src/main/java/com/example/java/HelloController.java
+++ src/main/java/com/example/java/HelloController.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class HelloController {
-    private   Logger log;
+
 
     @GetMapping("/orders")
     public List<Order> getOrders(@RequestHeader Map<String, String> RequestHeader) {
